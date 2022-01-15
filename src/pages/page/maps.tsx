15c906508090964bbe72bacb1eddff71b1362c3d
Luiz Filipe Bandeira <31023594+LuizzBanndera@users.noreply.github.com>
2022-01-15 18:29:15 -0300

    chore: added work in progress to maps

---
+++ src/pages/page/maps.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import WorkInProgress from "@components/workInProgress"
+
+export default function Maps() {
+
+  return(
+    <Container><WorkInProgress/></Container>
+  )
+}
+
+const Container = styled.div`
+  
+`
