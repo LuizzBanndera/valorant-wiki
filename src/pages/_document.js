95cbdd6ee07957e9f284ff320ab60bf779385c03
Luiz Filipe Bandeira <31023594+LuizzBanndera@users.noreply.github.com>
2022-01-18 09:45:04 -0300

    chore: changed default font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,7 +25,8 @@ export default class MyDocument extends Document {
         {this.props.styleTags}          
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"/>        
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
