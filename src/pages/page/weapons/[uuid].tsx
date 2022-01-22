168f23354b23f6d3c356d71550354013487dbdad
luizzbanndera <luizzbanndera@gmail.com>
2022-01-21 23:48:34 -0300

    chore

--- src/pages/page/weapons/[uuid].tsx
+++ src/pages/page/weapons/[uuid].tsx
@@ -10,53 +10,75 @@ export default function Weapon({data}: TWeaponData) {
 
   const category = data.category.substring(data.category.indexOf('::')+2)
   const wallPenetration = data.weaponStats.wallPenetration.substring(data.weaponStats.wallPenetration.indexOf('::')+2)
-  const fireMode = data.weaponStats.fireMode.substring(data.weaponStats.fireMode.indexOf('::')+2)
+
+  const handleStats = () => {
+    let wallPen
+    
+    switch (wallPenetration) {
+      case 'Low': wallPen = 20
+        break;
+      case 'Medium': wallPen = 50
+        break;
+      case 'High': wallPen = 90
+        break;
+      default: wallPen = 0
+        break;
+    }
+
+    const stats = {
+      fireRate: (data.weaponStats.fireRate * 7),
+      magSize: data.weaponStats.magazineSize,
+      wallPen,
+      reloadTime: (100-(data.weaponStats.reloadTimeSeconds * 18))
+    }
+
+    return stats
+  }
+
+  const stats = handleStats()
   
   return(
   <Container>
     <WeaponContainer>
       <div className="weapon-image">
-        <p className="g-red-title">{data.displayName+`.`}</p>
         <Image 
           quality={100} 
           priority 
           className="image" 
           src={data.displayIcon} 
-          alt="" 
+          alt="weapon" 
           layout="fill" 
           objectFit="contain"
         />
+        <div className="weapon-bg"/>
       </div>
       <div className="weapon-info">
+        <p className="g-red-title">{data.displayName+`.`}</p>
         <p className="g-title">{`//características`}</p>
         <div className="info-item">
-          <p className="g-label">nome:</p>
-          <p className="g-label">{data.displayName}</p>
+          <p className="g-label">categoria:</p>
+          <p className="g-label">{category}</p>
         </div>
         <div className="info-item">
-          <p className="g-label">categoria:</p>
+          <p className="g-label">valor:</p>
           <p className="g-label">{data.shopData.cost}</p>
         </div>                
         <div className="weapon-stats">
           <div className="info-stats">
             <p className="g-label">taxa de disparo:</p>                        
-            <Line percent={13} strokeColor="antiquewhite" trailColor="#0f1923"/>
+            <Line percent={stats.fireRate} strokeColor="antiquewhite" trailColor="#0f1923"/>
           </div>
           <div className="info-stats">
             <p className="g-label">tamanho do pente:</p>
-            <Line percent={10} strokeColor="antiquewhite" trailColor="#0f1923"/>
+            <Line percent={stats.magSize} strokeColor="antiquewhite" trailColor="#0f1923"/>
           </div>
           <div className="info-stats">
             <p className="g-label">penetração:</p>
-            <Line percent={100} strokeColor="antiquewhite" trailColor="#0f1923"/>
+            <Line percent={stats.wallPen} strokeColor="antiquewhite" trailColor="#0f1923"/>
           </div>
           <div className="info-stats">
             <p className="g-label">recarregamento:</p>
-            <Line percent={80} strokeColor="antiquewhite" trailColor="#0f1923"/>
-          </div>
-          <div className="info-stats">
-            <p className="g-label">zoom(ADS):</p>
-            <Line percent={100} strokeColor="antiquewhite" trailColor="#0f1923"/>
+            <Line percent={stats.reloadTime} strokeColor="antiquewhite" trailColor="#0f1923"/>
           </div>
         </div>
       </div>
@@ -73,18 +95,35 @@ const Container = styled.div`
   flex-wrap: wrap;
   height: 100%;
 `
-const WeaponContainer = styled.div`  
+const WeaponContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 60vw;
-  height: inherit;
   .weapon-image {
-    margin-left: 5rem;
-    height: 50%;
-    width: 40vw;
+    height: 15rem;  
+    max-width: 34rem;
     position: relative;
+    .image {
+      z-index: 1!important;
+    }
+
+    .weapon-bg {
+      position: relative;
+      height: 10rem;
+      background-color: var(--g-red);
+      width: 20rem;
+      bottom: -40px;
+      left: 143px;
+      z-index: 0;
+    }
+
   }
   .weapon-info {
     padding: 1rem;
     max-width: 536px;
+    .g-red-title {
+      margin-top: 0;
+    }
     .info-item {
       display: flex;
       flex-direction: row;
@@ -104,6 +143,9 @@ const WeaponContainer = styled.div`
         flex-direction: row;
         align-items: center;
         justify-content: space-between;
+        .g-label {
+          margin: 3px 0 3px;
+        }
         .rc-progress-line {
           height: 0.5rem;
           width: 20rem;
