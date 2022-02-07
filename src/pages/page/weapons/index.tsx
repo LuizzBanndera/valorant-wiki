9e1e20e99ec5bcc27c8dcaa3aa231692b182a3fc
Luiz Filipe Bandeira <31023594+luizfbalves@users.noreply.github.com>
2022-02-07 17:48:04 -0300

    feat: added new component

--- src/pages/page/weapons/index.tsx
+++ src/pages/page/weapons/index.tsx
@@ -2,13 +2,43 @@ import { GetStaticProps } from 'next'
 import db from '@services/api'
 import { AxiosResponse } from 'axios'
 import styled from 'styled-components'
-import CardWeapons from '@db/lib/components/cardWeapon';
-import { TWeapons } from '../../../lib/shared/types/types.weapons';
+import CardWeapons from '@db/lib/components/cardWeapon'
+import { TWeapons } from '../../../lib/shared/types/types.weapons'
+import CardWeaponGroup from '@components/cardWeaponGroup'
 
 export default function Weapons({data}: TWeapons) {
+  let uniqueCat = new Set()
 
+  let category : [{description: string, image: string}] = [{description: '', image: ''}]
+
+  const categoryData = data.map((weapon, idx) => {
+    return {
+      description: weapon.shopData?.categoryText || 'Combat',
+      image: weapon.displayIcon
+    }
+  })
+
+  category = categoryData.filter((value) => {
+    const isPresentInSet = uniqueCat.has(value.description)
+
+    uniqueCat.add(value.description)
+
+    return !isPresentInSet
+  })
+
+  console.log(category)
+  
+  
   return (
   <Container>
+    <div className="categories">
+    {      
+      category.map((cat, idx) => (        
+        <CardWeaponGroup key={idx} description={cat.description}/>
+        ))
+      }
+    </div>
+    <div className='items'>
     {
       data.map((weapon, idx) => (
         <div key={idx}>
@@ -16,15 +46,29 @@ export default function Weapons({data}: TWeapons) {
         </div>
       ))
     }
+    </div>
   </Container>)
 }
 
 const Container = styled.div`
+  .categories {  
+    display: flex;
+    position: sticky;
+    z-index: 5;
+    width: -webkit-fill-available;
+    justify-content: space-evenly;
+    border-bottom-style: groove;
+    background-color: var(--g-darkblue);
+    padding-bottom: 2rem;
+    border-width: 1px;   
+  }
+  .items {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   padding: 2rem 0 60px;
-  gap: 2rem;
+  gap: 2rem;  
+  }
 `
 
 export const getStaticProps : GetStaticProps = async ({locale}) => {
